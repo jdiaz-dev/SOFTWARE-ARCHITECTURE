db9b12882cffdc5d441a2f2cf4901d070bd37e14
Jonathan Alejandro <lordgelsin26@gmail.com>
2021-09-03 05:20:10 -0500

    CAP 8 MAPPING - the two-way mapping strategy

--- HANDS ON GUIDE - CLEAN ARCHITECTURE/8 mapping between boundaries/8 Mapping Between Boundaries.js
+++ HANDS ON GUIDE - CLEAN ARCHITECTURE/8 mapping between boundaries/8 Mapping Between Boundaries.js
@@ -1,7 +1,18 @@
 /*
   8. Mapping Between Boundaries
 
-    The “No Mapping” Strategy
+    **The “No Mapping” Strategy
       --no mapping between boundaries is completly valid especially when it is only a CRUD
       --many applications start with only a CRUD, later it grows and we can mapping between web and applications layer
+
+
+    **The “Two-Way” Mapping Strategy
+      --every layer map its content according its needs:
+        ♥ the web layer map to present data
+        ♥ the persistence layer map according to persistence model
+      --the advantage of this mapping strategy is that every layer has a model according to its needs
+      --the drawbacks is that due to mapping, it is produced a lot boilerplate code, besides to debug this mapping is pain
+
+
+
 */
