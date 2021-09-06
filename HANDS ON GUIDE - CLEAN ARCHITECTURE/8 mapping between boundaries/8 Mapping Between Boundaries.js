fbe6d07caec7d6e00e839c2cd09272d0a6873842
Jonathan Alejandro <lordgelsin26@gmail.com>
2021-09-06 05:23:27 -0500

    CAP 8 When to use which Mapping Strategy?

--- HANDS ON GUIDE - CLEAN ARCHITECTURE/8 mapping between boundaries/8 Mapping Between Boundaries.js
+++ HANDS ON GUIDE - CLEAN ARCHITECTURE/8 mapping between boundaries/8 Mapping Between Boundaries.js
@@ -34,5 +34,21 @@
 
       --this strategy play better if the models are similar because return without need to map
 
+    **When to use which Mapping Strategy?
+      --it depends
+      --we cannot set a hard and fast global rule
+      --we can start with a simple mapping strategy and later implement a complex strategy according the case
+      --the team should follow a guideline to apply a mapping strategy. The GUIDELINE should answer the question which mapping strategy should be the first choise in every situacion
+      --we might apply different mapping strategy in every layer
+
+
+      ···Example 1: If we use full mapping strategy in web and application layer, in order to decouple an use case from another, we don't have to deal with innecesary data in every use case
+      ···Example 2: in order to modify an use case the first choise is no mapping strateggy between web and application layuer to void mapping overhead, however when issue persistence is present, we move to two-way mapping strategy
+      ···Example 3: if we're working in a query no mapping stratey is the first choise between web and application layer and beetwen application and persistence layer to void mapping overhead, however when web or persistence issues is present we move to two-way mapping strategy
+
+      --the GUILDELINES should be present always in the mind of developer, the team should be checking constantly this guidelnes
+
+      check the detailed examples: pages 75 and 76
+
 
  */
---
+++ HANDS ON GUIDE - CLEAN ARCHITECTURE/8 mapping between boundaries/z-8 english words.js
@@ -0,0 +1,8 @@
+/*  8 mapping between boundaries
+        **When to use which Mapping Strategy?
+            untidy--desordenada
+            knowingly--a sabiendas
+            tidiness--orden
+            evolves--evoluciona
+
+*/
