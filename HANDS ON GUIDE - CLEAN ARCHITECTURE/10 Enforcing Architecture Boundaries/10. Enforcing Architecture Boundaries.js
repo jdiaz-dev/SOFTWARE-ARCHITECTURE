35e1d3532a9296d7e22635617fd7320c6fbf57bc
Jonathan Alejandro <lordgelsin26@gmail.com>
2021-09-25 07:37:48 -0500

    CHAPTER 10 Build Artifac

--- HANDS ON GUIDE - CLEAN ARCHITECTURE/10 Enforcing Architecture Boundaries/10. Enforcing Architecture Boundaries.js
+++ HANDS ON GUIDE - CLEAN ARCHITECTURE/10 Enforcing Architecture Boundaries/10. Enforcing Architecture Boundaries.js
@@ -13,6 +13,10 @@
         --however with subpackages it does not work, due to Java treat a subpackage as a external package
 
       **Post-Compile Checks
+        --we can run architecture test to check that Dependency Rule is being accomplish
+        --a tool to test if there are wrong dependencies is ArchUnit (JAVA)
+        --but with archUnit, these test are not infalible; if we make a  refactor the test will run and it will not find Dependecy Rule Violation
 
+      **Build Artifacts
         
 */
