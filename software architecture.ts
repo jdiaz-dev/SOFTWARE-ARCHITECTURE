a12d0f709cfc195441867e3a19f54bfb82b6305f
Jonathan Alejandro <lordgelsin26@gmail.com>
2022-09-28 05:24:42 -0500

    software architecture diagram

---
+++ software architecture.ts
@@ -0,0 +1,4 @@
+/* 
+    * software architecture: it is build different views of the project to differents members of the bussiness
+        - c4 and component diagrams are used to build software architecture
+*/
